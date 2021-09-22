58c82d06f5e0d339674d7d4d0f7a76eb4e117786
Sinan Çakıroğlu <sosyalkur@gmail.com>
2021-09-22 19:45:06 +0300

    Update auto_opening.php

--- auto_opening.php
+++ auto_opening.php
@@ -1,62 +1,62 @@
 <?php
 
 // Mysql Çalışma Saatleri //
-    function WorksHours($Hours){
-        
-        $Works = explode(":", $Hours);
-        $RealHours = $Works[0].":".$Works[1];
-        
-        return $RealHours;
-    }
+function WorksHours($Hours){
 
-    // Gerçek Zaman Çalışma Saatleri Açık Kapalı Modu //
-    function HoursWork($datetime){
+    $Works = explode(":", $Hours);
+    $RealHours = $Works[0].":".$Works[1];
 
-        $opennow = explode(',', $datetime);
+    return $RealHours;
+}
 
-        if(!HoursSecond() < $opennow[1] || !HoursSecond() < $opennow[0]):
+// Gerçek Zaman Çalışma Saatleri Açık Kapalı Modu //
+function HoursWork($datetime){
 
-            if( $opennow[0] > WorksHours(date('H:i')) ){
-        
-                echo '<span class="badge badge-danger">K</span>';
-            }
-            
-            elseif( $opennow[1] < WorksHours(date('H:i')) ){
+    $opennow = explode(',', $datetime);
 
-                echo '<span class="badge badge-danger">K</span>';
-            }
+    if(!HoursSecond() < $opennow[1] || !HoursSecond() < $opennow[0]):
 
-            else{
+        if( $opennow[0] > WorksHours(date('H:i')) ){
 
-                echo '<span class="badge badge-info">A</span>';
-            }
-
-        Endif;
-
-        return $opennow[1];
-    }
-
-
-    // 60 Saniye Döngüsü //
-    function HoursSecond(){
-        
-        for ($i = 0; $i < 1440; $i++) {
-            
-           return Hours24($i);
-
+            echo '<span class="badge badge-danger">K</span>';
         }
-    }
 
+        elseif( $opennow[1] < WorksHours(date('H:i')) ){
 
-    // 24 Saat Zaman Döngüsü //
-    function Hours24($i){
-
-        $Hours = 0;
-
-        while($Hours++ < 24){
-
-            $Time24 = date('H:i',mktime($Hours,$i,0,1,1));
-            return $Time24;
+            echo '<span class="badge badge-danger">K</span>';
         }
-   
+
+        else{
+
+            echo '<span class="badge badge-info">A</span>';
+        }
+
+    Endif;
+
+    return $opennow[1];
+}
+
+
+// 60 Saniye Döngüsü //
+function HoursSecond(){
+
+    for ($i = 0; $i < 1440; $i++) {
+
+       return Hours24($i);
+
     }
+}
+
+
+// 24 Saat Zaman Döngüsü //
+function Hours24($i){
+
+    $Hours = 0;
+
+    while($Hours++ < 24){
+
+        $Time24 = date('H:i',mktime($Hours,$i,0,1,1));
+        return $Time24;
+    }
+
+}
