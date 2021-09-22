7513c9b275fae4e6c2c12d2ebdf9ec0b4c6f1e75
Sinan Çakıroğlu <sosyalkur@gmail.com>
2021-09-22 19:49:21 +0300

    Create opening_day.php

---
+++ opening_day.php
@@ -0,0 +1,40 @@
+<?php
+
+  // Çalışma Günleri //
+  function WorkDay($Wday){
+
+      $Days = array(
+
+          1 => 'Pazartesi   <i class="text-success icon-checkmark float-right"></i>',
+          2 => 'Salı        <i class="text-success icon-checkmark float-right"></i>',
+          3 => 'Çarşamba    <i class="text-success icon-checkmark float-right"></i>',
+          4 => 'Perşembe    <i class="text-success icon-checkmark float-right"></i>',
+          5 => 'Cuma        <i class="text-success icon-checkmark float-right"></i>',
+          6 => 'Cumartesi   <i class="text-success icon-checkmark float-right"></i>',
+          7 => 'Pazar       <i class="text-success icon-checkmark float-right"></i>'
+
+      );
+
+      $EndDays = $Days[$Wday];
+
+      /*if(!$EndDays == 'Cumartesi' || !$EndDays == 'Pazar'):
+          return '<i class="text-success icon-close"></i>';
+      Endif;*/
+
+      return $EndDays;
+  }
+
+
+  // Hangi Günlerde Çalışıyor //
+  function DayWork($Days, $html = null) {
+
+      $Days       =   explode(',',$Days);
+      $WorksDays  =   "";
+
+      foreach ($Days as $arr => $day){ 
+          $WorksDays.= WorkDay($day). $html;
+      }
+
+      return $WorksDays;
+  }
+
