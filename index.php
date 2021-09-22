a74059a0f1747e151ae600e5b1a1113314caf678
Sinan Çakıroğlu <sosyalkur@gmail.com>
2021-09-22 19:38:56 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php 
+
+// İmport Et //
+require_once ("auot_opening.php");
+
+// Çalışacak Fontsiyoun HoursWork( Mysql'den Gelen Veri $veri->acilis-saatleri yada $veri['acilis-saatleri'] );
+HoursWork('08:00,21:00');
