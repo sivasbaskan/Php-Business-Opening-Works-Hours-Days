5659a970bd0b6e6bb08e9ec41a2d35d0611167e0
Sinan Çakıroğlu <sosyalkur@gmail.com>
2021-09-22 19:52:48 +0300

    Update index.php

--- index.php
+++ index.php
@@ -3,5 +3,14 @@
 // İmport Et //
 require_once ("auot_opening.php");
 
-// Çalışacak Fontsiyoun HoursWork( Mysql'den Gelen Veri $veri->acilis-saatleri yada $veri['acilis-saatleri'] );
+// Açılış ve Kapanış Saatleri //
+// Çalışacak Fonksiyonun HoursWork( Mysql'den Gelen Veri $veri->acilis-saatleri yada $veri['acilis-saatleri'] );
 HoursWork('08:00,21:00');
+
+
+// Çalışma Günleri //
+// Çalışacak Fonksiyonun DayWork( Mysql'den Gelen Veri $veri->calisma-gunleri yada $veri['calisma-gunleri'], ' html kodunu boşluk yada başka bir şey için kullanın ');
+DayWork('1,2,3,4,5,6,7','<br>');
+
+// İletişim Numaram Bana Ulaşabilirsiniz 0533 195 69 10 / 0545 690 20 87 //
+
